95b55d41c8d383064b18e15ba2b0ca1a6a1da463
clivemisiko <clivemisiko@users.noreply.github.com>
2026-09-08 12:23:21 +0300

    update base

--- config/settings/base.py
+++ config/settings/base.py
@@ -153,7 +153,7 @@ CORS_ALLOWED_ORIGINS = [
     origin.strip()
     for origin in config(
         "CORS_ALLOWED_ORIGINS",
-        default="http://localhost:5173,http://127.0.0.1:5173,104.248.50.1"
+        default="http://localhost:5173,http://127.0.0.1:5173,http://104.248.50.1"
     ).split(",")
     if origin.strip()
 ]
