fbe86d944806059018110af15c7f4e5fe347cca3
clivemisiko <clivemisiko@users.noreply.github.com>
2026-09-10 11:07:08 +0300

    fix(security): add CSRF_TRUSTED_ORIGINS for https domain

--- config/settings/base.py
+++ config/settings/base.py
@@ -157,3 +157,12 @@ CORS_ALLOWED_ORIGINS = [
     ).split(",")
     if origin.strip()
 ]
+
+CSRF_TRUSTED_ORIGINS = [
+    origin.strip()
+    for origin in config(
+        "CSRF_TRUSTED_ORIGINS",
+        default="https://cliveauthor.duckdns.org,http://cliveauthor.duckdns.org,http://127.0.0.1:8080,http://localhost:8080"
+    ).split(",")
+    if origin.strip()
+]
